2859dca1a8b78af891d7667d63d94aa9a330c162
jesuscardenas <jacc.ingsistemas@gmail.com>
2017-05-15 01:13:33 +0000

    add database seeder changes

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(SolicitudesTableSeeder::class);
+        $this->call(PartidaTableSeeder::class);
+        $this->call(JuegoTableSeeder::class);
     }
 }
