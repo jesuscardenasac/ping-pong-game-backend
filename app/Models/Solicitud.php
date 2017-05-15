c22f6745fa09f0ff15a3b796ad2ded1a297e63d8
jesuscardenas <jacc.ingsistemas@gmail.com>
2017-05-15 00:33:10 +0000

    logica del modelo solicitud

--- app/Models/Solicitud.php
+++ app/Models/Solicitud.php
@@ -1,10 +1,29 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Solicitud extends Model
 {
-    //
+    public $timestamps = false;
+    protected $table = 'solicitudes';
+    protected $fillable = [
+        'id_user_emisor', 'id_user_receptor', 'estado',
+    ];
+    
+    public function id_user_emisor()
+    {
+        return $this->belongsTo('App\User','id_user_emisor','id');
+    }
+    
+    public function id_user_receptor()
+    {
+        return $this->belongsTo('App\User','id_user_receptor','id');
+    }
+    
+    public function id_juego()
+    {
+        return $this->hasOne('App\Models\Juego','id_juego','id');
+    }
 }
