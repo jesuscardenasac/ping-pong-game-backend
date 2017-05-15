0e01e8a1aae79fa07512d349dc0b29262eba565f
jesuscardenas <jacc.ingsistemas@gmail.com>
2017-05-15 00:32:25 +0000

    logica del modelo partida

--- app/Models/Partida.php
+++ app/Models/Partida.php
@@ -1,10 +1,17 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Partida extends Model
 {
-    //
+    protected $fillable = [
+        'id_juego', 'puntos_emisor', 'puntos_receptor', 'estado'
+    ];
+    
+    public function id_juego()
+    {
+        return $this->belongsTo('App\Models\Juego','id_juego','id');
+    }
 }
