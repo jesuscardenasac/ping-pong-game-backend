2a408a2e6a04ed221049646940ab7ef0689d232b
jesuscardenas <jacc.ingsistemas@gmail.com>
2017-05-15 00:31:09 +0000

    logica del modelo juego

--- app/Models/Juego.php
+++ app/Models/Juego.php
@@ -1,10 +1,23 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Juego extends Model
 {
-    //
+    public $timestamps = false;
+    protected $fillable = [
+        'id_solicitud', 'estado',
+    ];
+    
+    public function id_solicitud()
+    {
+        return $this->belongsTo('App\Models\Solicitud','id_solicitud','id');
+    }
+    
+    public function id_partida()
+    {
+        return $this->hasMany('App\Models\Partida','id_partida','id');
+    }
 }
